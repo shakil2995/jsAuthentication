e52efbbe7c2c427b6ba074dfa291ebf01b86c72c
Shakil <shakil2995@gmail.com>
2022-03-16 00:34:06 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,35 @@
 const express=require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose=require('mongoose');
 const app = express();
 app.use (express.static("paublic"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema ={
+    email:String,
+    password:String
+}
+const User =  new mongoose.model("User",userSchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get("/",function(req, res){
     res.render("home");
@@ -16,8 +41,36 @@ app.get("/login",function(req, res){
 app.get("/register",function(req, res){
     res.render("register");
 })
-
-
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    });
+    newUser.save(function(err){
+        if (!err){
+            res.render("secrets");
+        } else{
+            console.log(err);
+        }
+    })
+})
+app.post("/login",function(req,res){
+        const username=req.body.username;
+        const password= req.body.password;
+    User.findOne({email:username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        } else{
+            if(foundUser){
+                if(foundUser.password===password){
+                    res.render("secrets");
+                }
+            } else{
+               res.send("Invalid credential");
+            }
+        }
+    })
+})
 
 
 
