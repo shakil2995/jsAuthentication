f46b3d75db1484e532d944f2a3de01aacc8427de
Shakil <shakil2995@gmail.com>
2022-03-16 02:31:46 +0600

    implemented hash for security

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose=require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const app = express();
 app.use (express.static("paublic"));
 app.set('view engine','ejs');
@@ -16,24 +17,8 @@ const userSchema = new mongoose.Schema ({
     password:String
 });
 const secret = process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
+// userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
 const User =  new mongoose.model("User",userSchema);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.get("/",function(req, res){
@@ -48,7 +33,7 @@ app.get("/register",function(req, res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if (!err){
