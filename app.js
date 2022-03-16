199d9543396ea80d794a498e4ea6ac0b79145eb7
Shakil <shakil2995@gmail.com>
2022-03-16 13:18:19 +0600

    adding express session

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require('ejs');
 const mongoose=require('mongoose');
 // const encrypt = require('mongoose-encryption');
 // const md5 = require('md5');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const saltRounds =10;
 
 const app = express();
@@ -33,52 +33,52 @@ app.get("/login",function(req, res){
 app.get("/register",function(req, res){
     res.render("register");
 })
+// using session 
 app.post("/register",function(req,res){
-    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
-        const newUser = new User({
-            email:req.body.username,
-            password:hash
-        });
-        newUser.save(function(err){
-            console.log(hash);
-            if (!err){
-                res.render("secrets");
-            } else{
-                console.log(err);
-            }
-        })
-    })
 })
 app.post("/login",function(req,res){
-        const username=req.body.username;
-        const password= req.body.password;
-    User.findOne({email:username},function(err,foundUser){
-        if(err){
-            console.log(err);
-        } else{
-            if(foundUser){
-                bcrypt.compare(password,foundUser.password,function(err,result){
-                    if(result===true){
-                        res.render("secrets");
-                    }else{
-                        res.send("Invalid credential");
-                     }
-                })
-                
-            } 
-        }
-    })
 })
+// session end 
 
 
-
-
-
-
-
-
-
-
+// using bcrypt 
+// app.post("/register",function(req,res){
+//     bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+//         const newUser = new User({
+//             email:req.body.username,
+//             password:hash
+//         });
+//         newUser.save(function(err){
+//             console.log(hash);
+//             if (!err){
+//                 res.render("secrets");
+//             } else{
+//                 console.log(err);
+//             }
+//         })
+//     })
+// })
+// app.post("/login",function(req,res){
+//         const username=req.body.username;
+//         const password= req.body.password;
+//     User.findOne({email:username},function(err,foundUser){
+//         if(err){
+//             console.log(err);
+//         } else{
+//             if(foundUser){
+//                 bcrypt.compare(password,foundUser.password,function(err,result){
+//                     if(result===true){
+//                         res.render("secrets");
+//                     }else{
+//                         res.send("Invalid credential");
+//                      }
+//                 })
+                
+//             } 
+//         }
+//     })
+// })
+// bcrypt end 
 app.listen(3000,function(){
     console.log("server working on port 3000");
 });
